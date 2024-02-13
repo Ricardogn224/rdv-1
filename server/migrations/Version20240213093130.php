cab5f01249e2078a7e6d8b09992266e35ef69968
senex127 <antoine127chabernaud@gmail.com>
2024-02-13 11:43:49 +0100

    modif rdv

--- server/migrations/Version20240213093130.php
+++ server/migrations/Version20240213093130.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240202183219 extends AbstractMigration
+final class Version20240213093130 extends AbstractMigration
 {
     public function getDescription(): string
     {
