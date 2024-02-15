bdeee5f30fa860c999e8719e66a7b4f65e440466
Jerrinald <jerrinald95190@live.fr>
2024-02-15 18:38:44 +0100

    appointment current user

--- client/src/components/pages/Rdv_page.jsx
+++ client/src/components/pages/Rdv_page.jsx
@@ -5,6 +5,8 @@ import { useNavigate } from 'react-router-dom';
 
 function Rdv_page() {
 
+  const token = localStorage.getItem("jwtToken");
+
 
   const now = new Date();
     console.log(now);
@@ -16,20 +18,29 @@ function Rdv_page() {
 
         const fetchRdv = async () => {
           try {
-            const response = await fetch("http://localhost:8888/api/rdv");
+            const response = await fetch("http://localhost:8888/api/appointments", {
+              method: 'GET',
+              headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`,
+                // You may include other headers like authorization if needed
+              },
+            });
             if (!response.ok) {
               throw new Error("Erreur réseau");
             }
             const allRdv = await response.json();
-            const now = new Date();
+            // const now = new Date();
 
-            const upcomingRdv = allRdv.filter(
-              (rdv) => new Date(rdv.date) > now
-            );
-            const pastRdv = allRdv.filter((rdv) => new Date(rdv.date) <= now);
+            // const upcomingRdv = allRdv.filter(
+            //   (rdv) => new Date(rdv.date) > now
+            // );
+            // const pastRdv = allRdv.filter((rdv) => new Date(rdv.date) <= now);
 
-            setRdv(upcomingRdv);
-            setOldrdv(pastRdv);
+            console.log(allRdv)
+
+            setRdv(allRdv['hydra:member']);
+            // setOldrdv(pastRdv);
           } catch (error) {
             console.error("Erreur lors de la récupération des rdv:", error);
           }
@@ -159,34 +170,36 @@ function Rdv_page() {
               <p>Mes rendez-vous à venir </p>
             </div>
 
-            {rdv.map((rdv) => (
-              <>
-                <div className="encadre ma-20">
-                  <div className="propositionrdv">
-                    <div className="flex flex-row items-center">
-                      <img src={medecinImage} alt="" />
-                      <div className="text">
-                        <h4>{rdv.name}</h4>
-                        <p>{rdv.motif}</p>
+            {rdv && (
+              rdv.map((rdv) => (
+                <>
+                  <div className="encadre ma-20">
+                    <div className="propositionrdv">
+                      <div className="flex flex-row items-center">
+                        <img src={medecinImage} alt="" />
+                        <div className="text">
+                          <h4>{rdv.provisionEmployee.employee.firstname} {rdv.provisionEmployee.employee.lastname}</h4>
+                          <p>{rdv.provisionEmployee.provision.name}</p>
+                          <p>{rdv.motif}</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="propositionrdv">
+                      <div className="flex flex-row items-center">
+                        <div className="zone-vide"></div>
+                        <div className="text">
+                          <h4>Le détail de votre rendez-vous</h4>
+                          <p>{rdv.planningDoctor.date} à {rdv.planningDoctor.hour}</p>
+                        </div>
+                      </div>
+                      <div className="flex flex-row ">
+                        <button className="m-4">Annuler</button>
                       </div>
                     </div>
                   </div>
-                  <div className="propositionrdv">
-                    <div className="flex flex-row items-center">
-                      <div className="zone-vide"></div>
-                      <div className="text">
-                        <h4>Le détail de votre rendez-vous</h4>
-                        <p>{rdv.date}</p>
-                      </div>
-                    </div>
-                    <div className="flex flex-row ">
-                      <button className="m-4">Modifier</button>
-                      <button className="m-4">Annuler</button>
-                    </div>
-                  </div>
-                </div>
-              </>
-            ))}
+                </>
+              ))
+            )}
           </div>
         </div>
       </>
